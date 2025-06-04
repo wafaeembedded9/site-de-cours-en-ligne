499bc99950eee3a85ce9f2b681c3c245c0ba685c
wafaeembedded9 <ouafae.allay22@ump.ac.ma>
2025-06-05 01:56:31 +0200

    adjust planning for professor

---
+++ js/ModulePlanning.js
@@ -0,0 +1,8 @@
+        export default class ModulePlanning {
+        constructor(niveau, module, filliere) {
+            this.niveau = niveau; // String representing the level
+            this.module = module; // String representing the module
+            this.filliere = filliere; // String representing the field of study
+        }
+    }
+    
